5ac25bee30f929dcb2e27065bc9d1ec9f27c854d
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2019-03-07 11:12:00 +0100

    Handle lost connections gracefully, fixes #2.

--- pywco/client.py
+++ pywco/client.py
@@ -2,13 +2,18 @@ import asyncio
 import threading
 import time
 import websockets
+import websockets.exceptions
 import logging
 import msgpack
 import janus
+import blinker
 
 from .communicator import Communicator
 
 log = logging.getLogger(__name__)
+
+connection_lost = blinker.Signal()
+
 class Client(Communicator):
    
     def start_communication(self):
@@ -32,13 +37,21 @@ class Client(Communicator):
     async def producer_handler(self):
         message = await self.send_queue.async_q.get()
         message_string = msgpack.packb(message, default=self.encode_command, use_bin_type=True)
-        await self.websocket.send(message_string)        
-        self.send_queue.async_q.task_done()
+        try:
+            await self.websocket.send(message_string)        
+            self.send_queue.async_q.task_done()
+        except websockets.exceptions.ConnectionClosed as ex:
+            self.stop()
+            connnection_lost.send(self, exception=ex)
 
     async def consumer_handler(self):
-        async for received_string in self.websocket:
-            self.handle_message(received_string)
-
+        try:
+            async for received_string in self.websocket:
+                self.handle_message(received_string)
+        except websockets.exceptions.ConnectionClosed as ex:
+            self.stop()
+            connection_lost.send(self, exception=ex)
+    
     def stop(self):
         self.loop.call_soon_threadsafe(self.do_stop)
 
